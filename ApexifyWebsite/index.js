102abc585402cbdd842ea7b294b57eafd4098b4d
umairmairi891 <134926416+umairmairi891@users.noreply.github.com>
2025-10-05 12:33:58 +0500

    add these new projects

--- ApexifyWebsite/index.js
+++ ApexifyWebsite/index.js
@@ -1,6 +1,25 @@
 const firstPara=document.querySelector('.first');
+const secondPara=document.querySelector('.second');
+const thirdPara=document.querySelector('.third');
+const forthPara=document.querySelector('.forth');
+
 const btn=document.getElementById('btn')
+const btn2=document.getElementById('btn2')
+const btn3=document.getElementById('btn3')
+const btn4=document.getElementById('btn4')
 btn.addEventListener(('click'),()=>{
    firstPara.classList.toggle('first');
+}
+)
+btn2.addEventListener(('click'),()=>{
+   secondPara.classList.toggle('second');
+}
+)
+btn3.addEventListener(('click'),()=>{
+   thirdPara.classList.toggle('third');
+}
+)
+btn4.addEventListener(('click'),()=>{
+   forthPara.classList.toggle('forth');
 }
 )
